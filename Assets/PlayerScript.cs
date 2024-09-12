8044a225e7d88403c2478ac0e3586876be121aaa
walkerGiavanna <94197876+walkerGiavanna@users.noreply.github.com>
2024-09-12 11:25:55 -0400

    Assignment2

---
+++ Assets/PlayerScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xValue = Input.GetAxis("Horizontal") * Time.deltaTime;
+        float zValue = Input.GetAxis("Vertical") * Time.deltaTime;
+        transform.Translate(xValue, 0, zValue);
+        
+    }
+}
